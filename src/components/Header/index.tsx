59f5a885065f1fa96cefbc8644b93bcf789b07a3
saur0w <sthapliyal085@gmail.com>
2026-02-28 12:56:05 +0530

    'Nav Bar Animation'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,94 +1,131 @@
 "use client";
 
-import React, { useRef, useState, useEffect } from "react";
-import { gsap } from "gsap";
 import styles from "./style.module.scss";
+import Link from "next/link";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
 
-interface CursorPosition {
-    left: number;
-    width: number;
-    opacity: number;
-}
+gsap.registerPlugin(SplitText);
 
-interface TabProps {
-    children: React.ReactNode;
-    setPosition: React.Dispatch<React.SetStateAction<CursorPosition>>;
-}
+const NAV_ITEMS = [
+    { label: "Home", href: "/"},
+    { label: "About", href: "/"},
+    { label: "Projects", href: "/"},
+    { label: "Contact", href: "/"},
+]
 
-interface CursorProps {
-    position: CursorPosition;
-}
+export default function Header() {
+    const pillRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const paraRef = useRef<HTMLParagraphElement>(null);
+    const navRef = useRef<HTMLElement>(null);
 
+    const handleMouseEnter = (e: React.MouseEvent<HTMLLIElement>) => {
+        if (!pillRef.current) return;
 
-export default function SlideTabsExample(){
-    return (
-        <div className={styles.wrapper}>
-            <SlideTabs />
-        </div>
-    );
-};
+        const { offsetLeft, offsetWidth } = e.currentTarget;
+        const link = e.currentTarget.querySelector("a");
 
-
-
-const SlideTabs: React.FC = () => {
-    const [position, setPosition] = useState<CursorPosition>({
-        left: 0,
-        width: 0,
-        opacity: 0,
-    });
-
-    const handleMouseLeave = () => {
-        setPosition((prev) => ({ ...prev, opacity: 0 }));
-    };
-
-    return (
-        <ul className={styles.nav} onMouseLeave={handleMouseLeave}>
-            {["Home", "About", "Menu"].map((label) => (
-                <Tab key={label} setPosition={setPosition}>
-                    {label}
-                </Tab>
-            ))}
-            <Cursor position={position} />
-        </ul>
-    );
-};
-
-
-const Tab: React.FC<TabProps> = ({ children, setPosition }) => {
-    const ref = useRef<HTMLLIElement>(null);
-
-    const handleMouseEnter = () => {
-        if (!ref.current) return;
-        const { width } = ref.current.getBoundingClientRect();
-        setPosition({
-            left: ref.current.offsetLeft,
-            width,
+        gsap.to(pillRef.current, {
+            x: offsetLeft,
+            width: offsetWidth ,
             opacity: 1,
-        });
-    };
-
-    return (
-        <li ref={ref} className={styles.tab} onMouseEnter={handleMouseEnter}>
-            {children}
-        </li>
-    );
-};
-
-
-const Cursor: React.FC<CursorProps> = ({ position }) => {
-    const cursorRef = useRef<HTMLLIElement>(null);
-
-    useEffect(() => {
-        if (!cursorRef.current) return;
-
-        gsap.to(cursorRef.current, {
-            left: position.left,
-            width: position.width,
-            opacity: position.opacity,
-            duration: 0.25,
+            duration: 0.35,
             ease: "power2.out",
         });
-    }, [position]);
 
-    return <li ref={cursorRef} className={styles.cursor} />;
-};
+        if (link) {
+            gsap.set(link, {
+                color: "#000000",
+            })
+        }
+
+        const otherLinks = e.currentTarget.parentElement?.querySelectorAll("a");
+        otherLinks?.forEach((other) => {
+            if (other != link) {
+                gsap.set(other, { color: "#ffffff"})
+            }
+            }
+        )
+    };
+
+    const handleMouseLeave = () => {
+        if (!pillRef.current) return;
+
+        gsap.to(pillRef.current, {
+            opacity: 0,
+            duration: 0.3,
+        });
+    };
+
+    const mouseEnterHeader = () => {
+        if (!containerRef.current || !paraRef.current) return;
+
+        const tl = gsap.timeline();
+
+        tl.to(paraRef.current, {
+            y: "-40%",
+            opacity: 0,
+        }, 0);
+
+        tl.to(containerRef.current, {
+            width: "600px",
+            duration: 0.8,
+            ease: "power2.out"
+        }, 0);
+
+        tl.to(navRef.current, {
+            opacity: 1,
+        }, 0)
+    }
+
+    const onMouseLeaveHeader = () => {
+
+        const tl = gsap.timeline();
+
+        tl.to(containerRef.current, {
+            width: "110px",
+            duration: 0.8,
+            ease: "power2.out"
+        }, 0);
+
+        tl.to(paraRef.current, {
+            y: "0%",
+            opacity: 1,
+        }, 0);
+
+        tl.to(navRef.current, {
+            opacity: 0,
+        }, 0)
+    }
+
+    return (
+        <header className={styles.header}>
+            <div
+                className={styles.container}
+                ref={containerRef}
+                onMouseEnter={mouseEnterHeader}
+                onMouseLeave={onMouseLeaveHeader}
+            >
+                <p ref={paraRef}>Menu</p>
+                <nav ref={navRef}>
+                    <ul onMouseLeave={handleMouseLeave}>
+                        <div className={styles.pill} ref={pillRef} />
+                        {NAV_ITEMS.map((navLink) => (
+                            <li
+                                key={navLink.label}
+                                onMouseEnter={handleMouseEnter}
+                            >
+                                <Link href={navLink.href}>
+                                    {navLink.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
